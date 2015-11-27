8f49ec3b9a8bab62a92fb1c1fdac2df6368f5397
Ciaran Synnott <hello@synnott.co.uk>
2015-11-27 14:33:46 +0000

    Create Gins.php

---
+++ ProductIDs/Gins.php
@@ -0,0 +1,21 @@
+<?php
+        $gins = array(
+            (object) array("name" => "Bombay Saphire 1 Litre", "ids" => array(
+                    "tesco" => 252695240,
+                    "asda" => 512843,
+                    "waitrose" => 34657,
+                    "morrisons" => 217561011
+                )),
+            (object) array("name" => "Gordons 1 Litre", "ids" => array(
+                    "tesco" => 257546202,
+                    "asda" => 18929,
+                    "waitrose" => 31569,
+                    "morrisons" => 119500011
+                )),
+            (object) array("name" => "Tanqueray Gin 1 Litre", "ids" => array(
+                    "tesco" => 256585738,
+                    "asda" => 910001181333,
+                    "waitrose" => 112395,
+                    "morrisons" => 288057011
+                ))
+        );
